3fe54b6a733cd97149fbc832ac340ebbe599e24b
Kelvin Roman <kromandomenech@student.fullsail.edu>
2022-06-11 13:38:02 -0700

    made the screen not resizeable and took away the maxize box

--- Calculator UI/Calculator.cpp
+++ Calculator UI/Calculator.cpp
@@ -1,6 +1,5 @@
 #include "Calculator.h"
-
-Calculator::Calculator() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(200,300), wxSize(400,500))
+Calculator::Calculator() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(200,300), wxSize(400,500), (wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX)))
 {}
 
 Calculator::~Calculator()
