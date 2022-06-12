7a2abb342c07141bfbcec68910bff744fd6c1145
Kelvin Roman <kromandomenech@student.fullsail.edu>
2022-06-11 20:02:17 -0700

    Finished laying out all the button no label on buttons.

--- Calculator UI/Calculator.cpp
+++ Calculator UI/Calculator.cpp
@@ -1,6 +1,33 @@
 #include "Calculator.h"
-Calculator::Calculator() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(200,300), wxSize(400,500), (wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX)))
-{}
+Calculator::Calculator() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(200, 300), wxSize(400, 500), (wxDEFAULT_FRAME_STYLE & ~(wxRESIZE_BORDER | wxMAXIMIZE_BOX)))
+{
+	sizerHolder = new wxBoxSizer(wxVERTICAL);
+	textBox = new wxTextCtrl(this, 1, wxEmptyString, wxPoint(0, 0), wxSize(windowWidth, 100), wxTE_BESTWRAP | wxTE_RIGHT | wxTE_READONLY);
+	buttons = new wxButton * [rows * cols];
+	grid = new wxGridSizer(rows, cols, 0, 0);
+	
+	// pos will be used to determine what index we are in for the buttons array and the ID of the buttons.
+	int pos = 0;
+	for (int x = 0; x < rows; x++)
+	{
+		for (int y = 0; y < cols; y++)
+		{
+			pos = y * rows + x;
+			buttons[pos] = new wxButton(this, 11 + pos, wxEmptyString, wxDefaultPosition, wxSize(buttonWidth, buttonHeight));
+			grid->Add(buttons[pos], 1, wxEXPAND | wxALL);
+		}
+	}
+	
+	sizerHolder->Add(textBox);
+	sizerHolder->Add(new wxButton(this, 10, "CLEAR", wxDefaultPosition, wxSize(buttonWidth * 2 - 3, buttonHeight)), 0, wxALIGN_RIGHT);
+	sizerHolder->Add(grid);
+
+	SetSizer(sizerHolder);
+	sizerHolder->Layout();
+	
+}
 
 Calculator::~Calculator()
-{}
+{
+	delete[] buttons;
+}
--- Calculator UI/Calculator.h
+++ Calculator UI/Calculator.h
@@ -2,8 +2,19 @@
 #include "wx/wx.h"
 class Calculator : public wxFrame
 {
+private:
+	int rows = 4;
+	int cols = 5;
+	int buttonWidth = 80;
+	int buttonHeight = 75;
+	int windowWidth = 400;
+	int windowHeight = 500;
 public:
 	Calculator();
 	~Calculator();
+	wxBoxSizer* sizerHolder = nullptr;
+	wxTextCtrl* textBox = nullptr;
+	wxGridSizer* grid = nullptr;
+	wxButton** buttons;
 };
 
